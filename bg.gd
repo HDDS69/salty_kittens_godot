06a1052192bcd47d291428b494fa90396281a3bb
HDDS69 <malygindanil9@gmail.com>
2024-12-23 15:18:52 +0300

    update

--- bg.gd
+++ bg.gd
@@ -5,4 +5,6 @@ var speed = 25
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	scroll_offset.x -= speed * delta
+	pass
+	#scroll_offset.x -= speed * delta
+	#$ParallaxLayer.position.x -= speed
