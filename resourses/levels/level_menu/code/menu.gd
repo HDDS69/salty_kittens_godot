8bdd725664c62c9a801aed76676ad1a75e699250
HDDS69 <malygindanil9@gmail.com>
2025-10-02 23:14:04 +0300

    discord NOW
    добавление интеграции с дыскордом и фикс бага с исчезновением кота + не большая оптимизация платформ

--- resourses/levels/level_menu/code/menu.gd
+++ resourses/levels/level_menu/code/menu.gd
@@ -8,6 +8,16 @@ var platform = OS.get_name()
 
 
 func _ready() -> void:
+	DiscordRPC.app_id = 1423378584986456245 # Application ID
+	DiscordRPC.details = "релиза не будет"
+	DiscordRPC.state = "срать вечно"
+	DiscordRPC.large_image = "example_game" # Image key from "Art Assets"
+	DiscordRPC.large_image_text = "Try it now!"
+	DiscordRPC.small_image = "boss" # Image key from "Art Assets"
+	DiscordRPC.small_image_text = "Fighting the end boss! D:"
+	DiscordRPC.start_timestamp = int(Time.get_unix_time_from_system()) # "02:46 elapsed"
+# DiscordRPC.end_timestamp = int(Time.get_unix_time_from_system()) + 3600 # +1 hour in unix time / "01:00:00 remaining"
+	DiscordRPC.refresh() # Always refresh after changing the values!
 	if platform == "Android":
 		SavePoint.fullscreen = true
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
