a70280987caeaf4d3765debe7fa919ed98598258
HDDS69 <malygindanil9@gmail.com>
2025-10-03 18:01:30 +0300

    MUSIC

--- resourses/levels/level_menu/code/menu.gd
+++ resourses/levels/level_menu/code/menu.gd
@@ -8,27 +8,25 @@ var platform = OS.get_name()
 
 
 func _ready() -> void:
-	DiscordRPC.app_id = 1423378584986456245 # Application ID
-	DiscordRPC.details = "релиза не будет"
-	DiscordRPC.state = "срать вечно"
-	DiscordRPC.large_image = "example_game" # Image key from "Art Assets"
-	DiscordRPC.large_image_text = "Try it now!"
-	DiscordRPC.small_image = "boss" # Image key from "Art Assets"
-	DiscordRPC.small_image_text = "Fighting the end boss! D:"
-	DiscordRPC.start_timestamp = int(Time.get_unix_time_from_system()) # "02:46 elapsed"
-# DiscordRPC.end_timestamp = int(Time.get_unix_time_from_system()) + 3600 # +1 hour in unix time / "01:00:00 remaining"
-	DiscordRPC.refresh() # Always refresh after changing the values!
 	if platform == "Android":
 		SavePoint.fullscreen = true
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DiscordRPC.app_id = 1423378584986456245 # Application ID
+		#DiscordRPC.details = "релиза не будет"
+		DiscordRPC.state = "в меню"
+		DiscordRPC.large_image = "background" # Image key from "Art Assets"
+		DiscordRPC.large_image_text = "котята лого"
+		#DiscordRPC.small_image = "drone" # Image key from "Art Assets"
+		#DiscordRPC.small_image_text = "ломаю железяки"
+		DiscordRPC.start_timestamp = int(Time.get_unix_time_from_system()) # "02:46 elapsed"
+	#	 DiscordRPC.end_timestamp = int(Time.get_unix_time_from_system()) + 3600 # +1 hour in unix time / "01:00:00 remaining"
+		DiscordRPC.refresh() # Always refresh after changing the values!
 
 func _on_play_pressed():
-	click_sound.play()
+	#click_sound.play()
 	get_tree().change_scene_to_file("res://resourses/levels/level_1/code/level.tscn")
 
-func _on_autors_pressed() -> void:
-	click_sound.play()
-	get_tree().change_scene_to_file("res://resourses/levels/level_autors/autors_level.tscn")
 	
 func spawn():
 	var dup = player.duplicate()
@@ -36,10 +34,14 @@ func spawn():
 	self.get_parent().add_child(dup)
 
 
-func _on_setting_button_pressed() -> void:
+func _on_settings_button_pressed() -> void:
 	setting.show()
 	click_sound.play()
 
+func _on_authors_pressed() -> void:
+	click_sound.play()
+	get_tree().change_scene_to_file("res://resourses/levels/level_autors/autors_level.tscn")
 
-func _on_quite_pressed() -> void:
+
+func _on_quit_pressed() -> void:
 	get_tree().quit()
