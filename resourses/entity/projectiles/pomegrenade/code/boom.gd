6ad5a442971dcc3b13818990ba0febe96548ddb4
HDDS69 <malygindanil9@gmail.com>
2025-10-08 13:06:40 +0300

    upd

--- resourses/entity/projectiles/pomegrenade/code/boom.gd
+++ resourses/entity/projectiles/pomegrenade/code/boom.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 var costil = false
 
+#функция смерти
 func death():
 	$radius.monitoring = true
 	$Boom.visible = false
@@ -11,13 +12,16 @@ func death():
 	await $AnimatedSprite2D.animation_finished
 	queue_free()
 
+#по окончанию таймера вызывается функция смерти
 func _on_timer_timeout():
 	death()
 
+# проверяем тела которые находятся в радиусе взрыва и "убиваем" если есть в радиусе
 func _on_radius_body_entered(body):
 	if body.name != 'TileMapLayer' and body.name != "salty platform":
 		body.death()
-	
+
+#при соприкосновении к любому телу начинается 1секундный таймер
 func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
 	if costil == false :
 		costil = true
