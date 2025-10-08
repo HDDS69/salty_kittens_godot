63f10529ab072080d5bdfacb3c804de41734a9e1
HDDS69 <malygindanil9@gmail.com>
2025-10-08 18:01:37 +0300

    upd

--- resourses/entity/npcs/drone/code/drone.gd
+++ resourses/entity/npcs/drone/code/drone.gd
@@ -2,16 +2,20 @@ extends RigidBody2D
 
 enum states {idle,fly,attack,death}
 var state: states = states.idle
-var hp = 12313
+var hp = 3
 
 func _process(delta: float) -> void:
+	$ProgressBar.value = hp
 	if state == states.idle:
 		$"..".progress_ratio += delta * 0.1
 	elif state == states.death:
 		death()
+	elif state == states.attack:
+		attack()
 
 func death():
-	
+	gravity_scale = 1.0
+	inertia = 1.0
 	$AnimatedSprite2D.play("death")
 	await $AnimatedSprite2D.animation_finished
 	queue_free()
@@ -20,3 +24,10 @@ func damage(dmg):
 	hp -= dmg
 	if hp <= 0:
 		state = states.death
+
+func attack():
+	pass
+
+func _on_area_2d_2_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		state = states.attack
