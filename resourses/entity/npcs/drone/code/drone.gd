4eb0dc2f42e2053abe5e6be2eb8ee1902790783a
HDDS69 <malygindanil9@gmail.com>
2025-10-09 22:33:04 +0300

    suck

--- resourses/entity/npcs/drone/code/drone.gd
+++ resourses/entity/npcs/drone/code/drone.gd
@@ -1,8 +1,11 @@
 extends RigidBody2D
 
-enum states {idle,fly,attack,death}
+@export var bullet : PackedScene
+enum states {idle,fly,attack,death,recharge,attack_timer,recharge_timer}
 var state: states = states.idle
 var hp = 3
+var count = 0
+var can_shoot = true
 
 func _process(delta: float) -> void:
 	$ProgressBar.value = hp
@@ -11,7 +14,11 @@ func _process(delta: float) -> void:
 	elif state == states.death:
 		death()
 	elif state == states.attack:
-		attack()
+		pass
+	elif state == states.recharge:
+		state = states.recharge_timer
+		$recharge.start()
+		
 
 func death():
 	gravity_scale = 1.0
@@ -26,8 +33,20 @@ func damage(dmg):
 		state = states.death
 
 func attack():
-	pass
-
+	var b = bullet.instantiate()
+	get_tree().root.add_child(b)
+	b.transform = $muzzle/Marker2D.global_transform
+	
 func _on_area_2d_2_body_entered(body: Node2D) -> void:
 	if body.name == "player":
 		state = states.attack
+		
+func _on_area_2d_2_body_exited(body: Node2D) -> void:
+	if body.name == "player":
+		state = states.idle
+
+func _on_timer_timeout() -> void:
+	state = states.attack
+
+func _on_time_to_shot_timeout() -> void:
+	attack()
