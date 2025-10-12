dc68d87e156009941078347fcd793aee51131f2b
HDDS69 <malygindanil9@gmail.com>
2025-10-12 17:17:42 +0300

    com

--- resourses/entity/npcs/drone_ram/code/drone_ram.gd
+++ resourses/entity/npcs/drone_ram/code/drone_ram.gd
@@ -8,30 +8,38 @@ func _process(delta: float) -> void:
 	if state == states.idle:
 		pass
 	elif state == states.attack:
-		linear_velocity.x = 7500 * delta
-
+		linear_velocity.x = 150 #7500 * delta
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.name == "player":
+	if body.name == "player" and state == states.idle:
 		if state != states.wait:
 			state = states.wait
 			$Timer.start(5)
-			
-
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.name == "player" and state != states.attack and state != states.death:
 		state = states.idle
 	
-
-
 func _on_timer_timeout() -> void:
 	state = states.attack
 
 func damage(dmg):
 	hp -= dmg
+	if hp <= 0:
+		state = states.death
+		death()
 
+func death():
+	$Area2D3.monitoring = true
+	#queue_free()
 
 func _on_area_2d_2_body_entered(body: Node2D) -> void:
 	if (body.name == 'player' or body.name == 'TileMapLayer') and state == states.attack:
+		death()
+
+
+func _on_area_2d_3_body_entered(body: Node2D) -> void:
+	print(body.name)
+	if body.name != 'TileMapLayer' and body.name != "salty platform":
+		body.damage(1)
 		queue_free()
