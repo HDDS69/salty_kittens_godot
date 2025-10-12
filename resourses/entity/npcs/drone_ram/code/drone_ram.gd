5f393048845d808f02781af08806db90dbd0e577
HDDS69 <malygindanil9@gmail.com>
2025-10-12 16:05:23 +0300

    upd

--- resourses/entity/npcs/drone_ram/code/drone_ram.gd
+++ resourses/entity/npcs/drone_ram/code/drone_ram.gd
@@ -2,39 +2,36 @@ extends RigidBody2D
 
 enum states {idle,attack,wait,death}
 var state: states = states.idle
+var hp = 5
 
 func _process(delta: float) -> void:
 	if state == states.idle:
 		pass
-	elif  state == states.attack:
-		pass
+	elif state == states.attack:
+		linear_velocity.x = 7500 * delta
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "player":
 		if state != states.wait:
 			state = states.wait
-			print(1)
 			$Timer.start(5)
 			
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body.name == "player" and state == states.wait:
+	if body.name == "player" and state != states.attack and state != states.death:
 		state = states.idle
 	
 
 
 func _on_timer_timeout() -> void:
 	state = states.attack
-	print(2)
+
+func damage(dmg):
+	hp -= dmg
 
 
-func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
-	if state == states.attack:
-		print(12)
-
-
-func _on_body_entered(body: Node) -> void:
-	if state == states.attack:
-		print(12)
+func _on_area_2d_2_body_entered(body: Node2D) -> void:
+	if (body.name == 'player' or body.name == 'TileMapLayer') and state == states.attack:
+		queue_free()
