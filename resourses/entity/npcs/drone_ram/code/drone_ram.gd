779cc0d0a35b090726f710c46d39230a4abb43fc
HDDS69 <malygindanil9@gmail.com>
2025-10-12 15:15:59 +0300

    com

--- resourses/entity/npcs/drone_ram/code/drone_ram.gd
+++ resourses/entity/npcs/drone_ram/code/drone_ram.gd
@@ -6,16 +6,35 @@ var state: states = states.idle
 func _process(delta: float) -> void:
 	if state == states.idle:
 		pass
+	elif  state == states.attack:
+		pass
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "player":
-		pass
+		if state != states.wait:
+			state = states.wait
+			print(1)
+			$Timer.start(5)
+			
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.name == "player" and state == states.wait:
+		state = states.idle
+	
 
 
 func _on_timer_timeout() -> void:
-	pass # Replace with function body.
+	state = states.attack
+	print(2)
+
+
+func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
+	if state == states.attack:
+		print(12)
+
+
+func _on_body_entered(body: Node) -> void:
+	if state == states.attack:
+		print(12)
