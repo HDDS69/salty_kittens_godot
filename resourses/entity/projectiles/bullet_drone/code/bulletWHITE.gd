4a07c6650aca82ba166b176df44427eea3682e0f
HDDS69 <malygindanil9@gmail.com>
2025-11-23 13:53:23 +0300

    МНОГО UPD

--- resourses/entity/projectiles/bullet_drone/code/bulletWHITE.gd
+++ resourses/entity/projectiles/bullet_drone/code/bulletWHITE.gd
@@ -19,7 +19,7 @@ func _physics_process(_delta: float) -> void:
 
 func _on_body_entered(body):
 	if body.name == 'player':
-		body.health -=1
+		body.damage(1)
 		queue_free()
 	if body.name == 'TileMapLayer':
 		queue_free()
