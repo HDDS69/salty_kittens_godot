f60d9293ba22dbcb3e9a71123594a982e9a26412
HDDS69 <malygindanil9@gmail.com>
2025-11-23 22:33:36 +0300

    upd

--- resourses/entity/objects/stone/code/stone.gd
+++ resourses/entity/objects/stone/code/stone.gd
@@ -1,35 +1,29 @@
 extends RigidBody2D
-@export var stone : PackedScene
 
-#@onready var player = $"../player"
 @onready var ui = $ui
 @onready var anim = $AnimatedSprite2D
+@onready var kil_zone = $Area2D
 var x = 0
-var duck = false
-#func _process(_delta: float) -> void:
-	##print(linear_velocity)
 
 func damage(_dmg):
 	x = randfn(-500,500)
 	apply_impulse(Vector2(x,-600))
-	duck = true
-	#var stone_1 = stone.instantiate()
-	#stone_1.directionFALL = -1d
-	#add_child(stone_1)
-	
-#func _on_body_entered(body):
-	#if body.name == "player":
-		#ui.show()
-#
-#func _on_body_exited(body):
-	#if body.name == "player":d
-		#ui.hide()
-
+	kil_zone.monitoring = true
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if duck and body.name != 'TileMapLayer' and self != body:
+	if body.name != 'TileMapLayer' and self != body and body.name != 'player':
 		body.damage(100)
 		anim.play('death')
 		await anim.animation_finished
 		queue_free()
-		
+	kil_zone.monitoring = false
+
+
+func _on_ent_body_exited(body: Node2D) -> void:
+	if body.name == "player":
+		ui.hide()
+
+
+func _on_ent_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		ui.show()
