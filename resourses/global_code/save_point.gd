6e203dcb35c97348cce1a09c21e32262cc7ddcfd
GenosixGD <genosixgd@gmail.com>
2026-05-14 19:40:03 +0400

    Изменил комментарии Добавил TODO и убрал ненужную закоментированную функцию _delta

--- resourses/global_code/save_point.gd
+++ resourses/global_code/save_point.gd
@@ -1,13 +1,10 @@
 extends Node
 # глобальная переменная она позже будет сохранятся здесь и загружаться куда надо
+#TODO: Сохранение этих переменных
+
 var mobile_control = false
 var fullscreen = false
 var light = true
 var vsinc = true
 var inventory = []
 
-#
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
