d2efbd8d9dae962c8811a84f62ab25bbb7590e84
HDDS69 <malygindanil9@gmail.com>
2026-09-11 21:05:15 +0300

    bar upd2

--- resourses/levels/level_2/code/mini_lamp.gd
+++ resourses/levels/level_2/code/mini_lamp.gd
@@ -1,10 +1,18 @@
 extends RigidBody2D
-@export var node : Node2D
+@export var node : Node
+@onready var pin = $PinJoint2D
+var hp = 3
 
+func _ready() -> void:
+	pin.node_a = node.get_path()
+	
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if 'velocity' in body:
 		self.apply_force(body.velocity*5)
 
 func damage(dmg):
+	hp -=1
+	if hp <= 0:
+		queue_free()
 	apply_force(Vector2(0,dmg)*10000)
